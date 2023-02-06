7a53a0885efece98d319c8e962ccde10859bac0c
mauroph1 <122278388+mauroph1@users.noreply.github.com>
2023-02-06 10:49:33 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public double saldo;
+
+        public string titular;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public bool Transferir(double valor,ContaCorrente destino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+
+        }
+
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular :" + titular);
+            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Número Agência :" + numero_agencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+
+
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using ByteBank;
+
+ContaCorrente contaDoWayne = new ContaCorrente();
+contaDoWayne.titular = "Bruce Wayne";
+contaDoWayne.numero_agencia = 15;
+contaDoWayne.conta = "1010-x";
+contaDoWayne.saldo = 30000000000;
+
+Console.WriteLine("Saldo da conta do Bruce Wayne é = " + contaDoWayne.saldo);
+
+contaDoWayne.Depositar(150);
+Console.WriteLine("Saldo da conta do Bruce Wayne após o depósito é de = " + contaDoWayne.saldo);
+
+if (contaDoWayne.Sacar(300) == true)
+{
+    Console.WriteLine("Saldo da conta do Bruce Wayne  após o saque é de = " + contaDoWayne.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para este saque.");
+}
+
+ContaCorrente contaDoStark = new ContaCorrente();
+contaDoStark.titular = "Tony Stark";
+contaDoStark.numero_agencia = 17;
+contaDoStark.conta = "2020-x";
+contaDoStark.saldo = 29999999750;
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Saldo Bruce Wayne =" + contaDoWayne.saldo);
+Console.WriteLine("Saldo Tony Stark=" + contaDoStark.saldo);
+
+contaDoWayne.Transferir(50, contaDoStark);
+Console.WriteLine( "Transferência de R$50,00 efetuada com sucesso!");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("Saldo Bruce Wayne =" + contaDoWayne.saldo);
+Console.WriteLine("Saldo Tony Stark=" + contaDoStark.saldo);
+
+
+ContaCorrente contaDoLuthor = new ContaCorrente();
+
+Console.WriteLine(contaDoLuthor.titular);
+Console.WriteLine(contaDoLuthor.saldo);
+Console.WriteLine(contaDoLuthor.conta);
+
+
+Console.WriteLine(contaDoWayne.ExibirDadosDaConta);
+
+
